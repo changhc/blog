d46c90db194f74bfde6cecced6be00fef59e7e34
Huan-Cheng Chang <changhc84@gmail.com>
2021-09-14 08:13:15 +0200

    lint

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,11 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
+  // DropCap.js
+  var dropcaps = document.querySelectorAll(".dropcap");
+  window.Dropcap.layout(dropcaps, 2);
 
-	// DropCap.js
-	var dropcaps = document.querySelectorAll(".dropcap");
-	window.Dropcap.layout(dropcaps, 2);
-
-	// Round Reading Time
-    $(".time").text(function (index, value) {
-      return Math.round(parseFloat(value));
-    });
-
+  // Round Reading Time
+  $(".time").text(function (index, value) {
+    return Math.round(parseFloat(value));
+  });
 });
-
-
